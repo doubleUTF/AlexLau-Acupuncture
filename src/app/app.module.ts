e0bfe28ab86a76ba1e31fce19298dfda931e72c8
doubleUTF <dlau373@gmail.com>
2017-02-08 17:33:31 -0800

    Implementing routing for appointments route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BootstrapModule } from './bootstrap/bootstrap.module';
 import { AppointmentsComponent } from './appointments/appointments.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './appointments/schedule/schedule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AppointmentsComponent
+    AppointmentsComponent,
+    HomeComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    BootstrapModule
+    BootstrapModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
