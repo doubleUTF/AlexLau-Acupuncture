db25796ef1afb0115e0da40140a3c810504ca80e
BigD <dlau373@gmail.com>
2017-07-08 00:43:54 -0700

    Added a maxAge object to express app static line

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public'),{ maxAge: 86400000 }));
 app.set('view engine','hbs');
 
 // Uncomment to allow cross server setup
