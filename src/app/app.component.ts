8a86fc064719c904d9a11424743dd1c17393477a
BigD <dlau373@gmail.com>
2017-07-09 22:50:10 -0700

    Added google analytics script into index.html. Appointment page has reworked so mobile screens will launch a new tab whereas full screens will utilize iframe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
+  constructor(public router: Router) {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          ga('set', 'page', event.urlAfterRedirects);
+          ga('send', 'pageview');
+        }
+      });
+    }
   }
