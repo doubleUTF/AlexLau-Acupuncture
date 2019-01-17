057da13143d18029a25b64638d314cff032c264e
BigD <dlau373@gmail.com>
2019-01-17 14:04:35 -0800

    removed console log from metasrevice

--- src/app/meta-service.service.ts
+++ src/app/meta-service.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
- 
+
 @Injectable({
    providedIn: 'root'
 })
- 
-export class MetaService { 
+
+export class MetaService {
    constructor(@Inject(DOCUMENT) private dom) { }
-   
+
    link: HTMLLinkElement = this.dom.createElement('link');
    createCanonicalURL() {
       this.link.setAttribute('rel', 'canonical');
@@ -18,4 +18,4 @@ export class MetaService {
    deleteCanonicalURL(){
       this.dom.head.removeChild(this.link);
    }
-} 
+}
